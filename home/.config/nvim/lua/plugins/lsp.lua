96dc5c1a66d3c749975f56861dcbc864537847e4
Justin Dhillon <justin.singh.dhillon@gmail.com>
2024-02-12 13:30:43 -0800

    fix lsp.lua for neovim

--- home/.config/nvim/lua/plugins/lsp.lua
+++ home/.config/nvim/lua/plugins/lsp.lua
@@ -1,68 +1,30 @@
 return {
 	{
-		'VonHeikemen/lsp-zero.nvim',
-		branch = 'v3.x',
-		lazy = true,
-		config = false,
-		init = function()
-			-- Disable automatic setup, we are doing it manually
-			vim.g.lsp_zero_extend_cmp = 0
-			vim.g.lsp_zero_extend_lspconfig = 0
+		'hrsh7th/nvim-cmp',
+		config = function(cmp)
+			local cmp = require('cmp')
+			cmp.setup({
+				completion = { completeopt = 'menu,menuone,noinsert' },
+				mapping = cmp.mapping.preset.insert(),
+				sources = cmp.config.sources({{ name = 'nvim_lsp' }}),
+			})
 		end,
 	},
-
-	-- Autocompletion
 	{
-		'hrsh7th/nvim-cmp',
-		event = 'InsertEnter',
-		dependencies = {
-			{'L3MON4D3/LuaSnip'},
-		},
+		'neovim/nvim-lspconfig', 
+		dependencies = { 'hrsh7th/cmp-nvim-lsp' },
 		config = function()
-			-- Here is where you configure the autocompletion settings.
-			local lsp_zero = require('lsp-zero')
-			lsp_zero.extend_cmp()
-
-			-- And you can configure cmp even more, if you want to.
-			local cmp = require('cmp')
-			local cmp_action = lsp_zero.cmp_action()
-
-			cmp.setup({
-				formatting = lsp_zero.cmp_format(),
-				mapping = cmp.mapping.preset.insert({
-					['<C-Space>'] = cmp.mapping.complete(),
-					['<C-u>'] = cmp.mapping.scroll_docs(-4),
-					['<C-d>'] = cmp.mapping.scroll_docs(4),
-					['<C-f>'] = cmp_action.luasnip_jump_forward(),
-					['<C-b>'] = cmp_action.luasnip_jump_backward(),
+			local lsp_capabilities = require('cmp_nvim_lsp').default_capabilities()
+			local lsp_on_attach = function(client, bufnr)
+				local bufopts = { noremap=true, silent=true, buffer=bufnr }
+			end
+			local lspconfig = require('lspconfig')
+			for _, server in pairs({ 'eslint', 'tsserver' }) do
+				lspconfig[server].setup({
+					capabilities = lsp_capabilities,
+					on_attach = lsp_on_attach,
 				})
-			})
-		end
+			end
+		end,
 	},
-
-	-- LSP
-	{
-		'neovim/nvim-lspconfig',
-		cmd = 'LspInfo',
-		event = {'BufReadPre', 'BufNewFile'},
-		dependencies = {
-			{'hrsh7th/cmp-nvim-lsp'},
-		},
-		config = function()
-			-- This is where all the LSP shenanigans will live
-			local lsp_zero = require('lsp-zero')
-			lsp_zero.extend_lspconfig()
-
-			lsp_zero.on_attach(function(client, bufnr)
-				-- see :help lsp-zero-keybindings
-				-- to learn the available actions
-				lsp_zero.default_keymaps({buffer = bufnr})
-			end)
-
-			-- (Optional) Configure lua language server for neovim
-			local lua_opts = lsp_zero.nvim_lua_ls()
-			require('lspconfig').lua_ls.setup(lua_opts)
-			require('lspconfig').tsserver.setup({})
-		end
-	}
 }
